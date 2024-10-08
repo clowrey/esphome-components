2dbdac577006318481fd0db71bd8c67b12370a59
Garag <garag@everwait.de>
2024-10-08 20:10:08 +0200

    always redraw complete display

--- components/axs15231/display/axs15231_display.cpp
+++ components/axs15231/display/axs15231_display.cpp
@@ -308,14 +308,16 @@ void AXS15231Display::display_() {
   }
 
   // we will only update the changed rows to the display
-  size_t const w = this->x_high_ - this->x_low_ + 1;
-  size_t const h = this->y_high_ - this->y_low_ + 1;
+  size_t const w = this->width_; //this->x_high_ - this->x_low_ + 1;
+  size_t const h = this->height_; //this->y_high_ - this->y_low_ + 1;
   size_t const x_pad = this->get_width_internal() - w - this->x_low_;
-  this->set_addr_window_(this->x_low_, this->y_low_, this->x_high_, this->y_high_);
+  this->set_addr_window_(1, 1, this->width_, this->height_);
+//  this->set_addr_window_(this->x_low_, this->y_low_, this->x_high_, this->y_high_);
 
   this->enable();
 
-  if (this->x_low_ == 0 && this->y_low_ == 0 && x_pad == 0) {
+//  if (this->x_low_ == 0 && this->y_low_ == 0 && x_pad == 0) {
+  if (true) {
     this->write_cmd_addr_data(8, 0x32, 24, 0x2C00, this->buffer_, w * h * 2, 4);
   } else {
     this->write_cmd_addr_data(8, 0x32, 24, 0x2C00, nullptr, 0, 4);
